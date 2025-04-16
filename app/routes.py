20543a2383e72cf0496b9f4a14db0c0fe59d552c
LiangYujunyi-2425 <240002257@stu.vtc.edu.hk>
2025-04-16 16:11:25 +0000

    new

--- app/routes.py
+++ app/routes.py
@@ -189,3 +189,8 @@ def unfollow(username):
     db.session.commit()
     flash(_('You are not following %(username)s.', username=username))
     return redirect(url_for('user', username=username))
+
+@app.route('/partnership_benefits', methods=['GET', 'POST'])
+@login_required
+def partnership_benefits():
+        return render_template('partnership_benefits.html.j2',title=_('partnership_benefits'))

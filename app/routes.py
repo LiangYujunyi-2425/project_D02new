46a82a84a9b774ecd7ef2fe329e1d5019c6a7e4d
LiangYujunyi-2425 <240002257@stu.vtc.edu.hk>
2025-04-19 15:29:22 +0000

    newppp

--- app/routes.py
+++ app/routes.py
@@ -9,34 +9,6 @@ from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, \
 from app.models import User, Post ,Mobile_c,Health_care,Voice_c
 from app.email import send_password_reset_email
 
-def nimabi():
-    app_context = app.app_context()
-    app_context.push()
-    db.drop_all()
-    db.create_all()
-
-    u1 = User(username='john', email='john@example.com')
-    u2 = User(username='susan', email='susan@example.com')
-    u1.set_password("P@ssw0rd")
-    u2.set_password("P@ssw0rd")
-    db.session.add(u1)
-    db.session.add(u2)
-    u1.follow(u2)
-    u2.follow(u1)
-
-    p1 = Post(body='my first post!', author=u1)
-    p2 = Post(body='my first post!', author=u2)
-    db.session.add(p1)
-    db.session.add(p2)
-
-    m1 = Mobile_c(id = 10001,body = "流動通訊")
-    v1 = Voice_c(id = 1002,body = "語音電話")
-    h1 = Health_care(id =10001,body = "醫療保健")
-    db.session.add(m1)
-    db.session.add(v1)
-    db.session.add(h1)
-
-    db.session.commit()
 
 @app.before_request
 def before_request():
@@ -53,6 +25,12 @@ def index():
     form = PostForm()
     if form.validate_on_submit():
         post = Post(body=form.post.data, author=current_user)
+        m1 = Mobile_c(id = 10001,body = "流動通訊")
+        v1 = Voice_c(id = 1002,body = "語音電話")
+        h1 = Health_care(id =10001,body = "醫療保健")
+        db.session.add(m1)
+        db.session.add(v1)
+        db.session.add(h1)
         db.session.add(post)
         db.session.commit()
         flash(_('Your post is now live!'))
